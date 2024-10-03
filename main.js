58e01901074fd5b80a55ca32c335086e43b3bcdc
FrancescoMarra97 <francescomarra192@gmail.com>
2024-10-03 15:16:53 +0200

    human code and promp

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+/* Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, 
+stampa un messaggio appropriato sull’esito del controllo.
+ */
 
+//passo 1 chiedere all'utente la sua email
+let mail=prompt("inserisci la tua email")
+
+//Passo 2 creare una lista di email, conm un array
+const list_mail[
+    
+]
+
+//passo 3 effettuare il controllo sulla lista
+
+
+//passo 4 effettuare un controllo sulla lista 
