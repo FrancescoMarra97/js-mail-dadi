85386a68eb9c2a548aa2e379fb3c54d833be12cc
FrancescoMarra97 <francescomarra192@gmail.com>
2024-10-03 15:27:54 +0200

    add array

--- main.js
+++ main.js
@@ -6,11 +6,16 @@ stampa un messaggio appropriato sull’esito del controllo.
 let mail=prompt("inserisci la tua email")
 
 //Passo 2 creare una lista di email, conm un array
-const list_mail[
-    
+const list_mail=[
+    "francescomarra@gmail.com",
+    "boolean@gmail.com",
+    "fabio@hotmail.com",
+    "barbara@gmail.com",
+    "anna@gmail.com"
 ]
+//passo 3 effettuare il controllo sull'inserimento corretto della stringa
 
-//passo 3 effettuare il controllo sulla lista
+//passo 4 effettuare il controllo sulla lista
 
 
-//passo 4 effettuare un controllo sulla lista 
+//passo 5 stampare un messaggio appropriato
